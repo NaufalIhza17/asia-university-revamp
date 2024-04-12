f30ffa1349cc779722a2c161a262352fc4314f3d
NaufalIhza17 <nawfal17syahzada@gmail.com>
2024-04-12 17:04:37 +0800

    feat: add side bar current and date hooks

---
+++ src/staticData/dashboardPath.ts
@@ -0,0 +1,26 @@
+export const dashboardPath = [
+  {
+    slug: "/dashboard",
+    name: "Dashboard",
+  },
+  {
+    slug: "/dashboard/payment",
+    name: "Payment Info"
+  },
+  {
+    slug: "/dashboard/tronclass",
+    name: "Tronclass"
+  },
+  {
+    slug: "/dashboard/transcript",
+    name: "Transcript"
+  },
+  {
+    slug: "/dashboard/news",
+    name: "AU News"
+  },
+  {
+    slug: "/logout",
+    name: "Logout"
+  },
+];
